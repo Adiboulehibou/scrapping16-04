ff73e28e45a5c5083dfeab0b372cfd30a32983de
Adiboulehibou <62951424+Adiboulehibou@users.noreply.github.com>
2020-04-17 08:21:38 +0200

    Add files via upload

--- lib/darktrader.rb
+++ lib/darktrader.rb
@@ -4,18 +4,14 @@ require 'pry'
 
 doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
 
-# def array_of_hash_crypto[(hash_crypto)]
-#   return array_of_hash_crypto << hash_crypto
-# end
 
 def hash_crypto(truk)
-	#print truk
-	prix = truk.css("td.cmc-table__cell--sort-by__price//a").map{|prx| prx.text} 
-	titre = truk.css("td.cmc-table__cell--sort-by__name//a").map(&:text) 
+	prix = truk.css("td.cmc-table__cell--sort-by__price//a").map{|prx| prx.text} # met dans un tableau les differents prix de cryptomonnais 
+	titre = truk.css("td.cmc-table__cell--sort-by__name//a").map(&:text) #met dans un tableau les nom des differentes cryptomonais
 	array_of_hash_crypto = []
-	(prix.length).times do |v|
+	(prix.length).times do |v|#une bouche permetant d'incrmenter un hash et d'y entrer en "key" les noms des cryptomonnais et en "valeurs" les prix
 		hash_crypto = {titre[v] => prix[v]}
-		array_of_hash_crypto << hash_crypto 
+		array_of_hash_crypto << hash_crypto #hash in array
 	end
 puts array_of_hash_crypto
 end
--- lib/mairiechristmas.rb
+++ lib/mairiechristmas.rb
@@ -3,25 +3,27 @@ require 'open-uri'
 require 'pry'
 
 
-def get_townhall_email(townhall_url)
-  email_mairie = townhall_url.css("table.table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(4) > td:nth-child(2)").map(&:text)
-  #puts email_mairie
-end
+
 
 
 
 doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
 
-def get_townhall_urls(townhall_all_url)
-  mairie_url = townhall_all_url.css("a.lientxt").map{ |link| link['href'] }
-  mairie_name = townhall_all_url.css("a.lientxt").map(&:text)
-  mairie_complete_url = []
-  array_of_hash_town_and_url = []
-  185.times do |v|
-    mairie_complete_url[v] = "http://annuaire-des-mairies.com"+mairie_url[v][1..-1]
-    u = Nokogiri::HTML(open(mairie_complete_url[v]))
-    hash_town_and_url = { mairie_name[v] => get_townhall_email( u )}
-    array_of_hash_town_and_url << hash_town_and_url
+def get_townhall_email(townhall_url)#methode permetant de scapper les emails des mairie sur leurs pages web
+  email_mairie = townhall_url.css("table.table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(4) > td:nth-child(2)").map(&:text)
+  #puts email_mairie
+end
+
+def get_townhall_urls(townhall_all_url)#methode permettant d'associer les nom des ville avec l'email de contact
+  mairie_url = townhall_all_url.css("a.lientxt").map{ |link| link['href'] }#met dans un tableau la fin des url de chaques site des mairis
+  mairie_name = townhall_all_url.css("a.lientxt").map(&:text)#permet de recuperer le nom de chaque commune
+  mairie_complete_url = []#initialisation d'un tableau pour stoker l'url complete des sites de chaque mairies
+  array_of_hash_town_and_url = []#array of hash
+  185.times do |v|#boucle increment 185 fois, soit le nombre de mairies dans le val d'oise
+    mairie_complete_url[v] = "http://annuaire-des-mairies.com"+mairie_url[v][1..-1]#permet de stoker l'url complete des site de chaque mairis
+    u = Nokogiri::HTML(open(mairie_complete_url[v]))#url des differente url complete des ville en fonction de "v"
+    hash_town_and_url = { mairie_name[v] => get_townhall_email( u )}#creation du hash: "key"= nom de la mairis et v"aleurs"=email de contact de la mairis
+    array_of_hash_town_and_url << hash_town_and_url#hash in array
   end
   puts array_of_hash_town_and_url
 
